424c3c8c541160aad099c825c7f7cade35198add
kiefer <kieferlau@gmail.com>
2026-08-21 13:06:12 +0800

    Rename chatbot to Peter and add greeting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Peter User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Peter.java
@@ -0,0 +1,21 @@
+public class Peter {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String banner = " ____       _            \n"
+                + "|  _ \\ ___ | |_ ___ _ __ \n"
+                + "| |_) / _ \\| __/ _ \\ '__|\n"
+                + "|  __/  __/| ||  __/ |   \n"
+                + "|_|   \\___| \\__\\___|_|   \n";
+
+        System.out.println(line);
+        System.out.print(banner);
+        System.out.println("My name is Peter");
+        System.out.println("How am I saving you today?");
+        System.out.println(line);
+        System.out.println("Bye! See you next time.");
+        System.out.println(line);
+
+
+
+    }
+}
